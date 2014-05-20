b3b25df951562cae177ff6ba89e0815fcc6efae5
thanhnhu <congthanhkid@gmail.com>
2014-05-20 15:46:24 +0700

    da xoa source

--- components/com_virtuemart/themes/default/templates/browse/includes/browse_pagenav.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' );
-mm_showMyFileName(__FILE__); ?>
-<?php if(!@is_object( $pagenav)) return;  ?>
-<!-- BEGIN PAGE NAVIGATION -->
-<div class="phantrang">
-	<?php $pagenav->writePagesLinks( $search_string ); ?>
-</div>
-<!-- END PAGE NAVIGATION -->
